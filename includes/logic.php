ec07a9152734aeff9d4dd7debdc95053da480e3b
schuwarz <schuwarz@outlook.com>
2025-01-10 21:16:11 +0500

    Randomate number project

---
+++ includes/logic.php
@@ -0,0 +1,11 @@
+<?php
+// logic.php
+$Jopa = null;
+$Loh = "Ahaha goofy";
+
+if (isset($_POST["Pipa"])) {
+    $Jopa = rand(1, 10);
+}
+//Jopa делает число, если нажата кнопка Pipa
+
+?>
--- index.php
+++ index.php
@@ -1,21 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My first PHP steps</title>
+  <title>Number randomatin'</title>
 </head>
+
 <body>
+
+  <h1>Try to randomate number 3 or 5 or 7!)))</h1>
+
+  <form method="POST">
+    <button type="submit" name="Pipa">Randomate a new nimber</button>
+  </form>
+  <!-- Кнопка с именем Pipa, которая отправляет данные о нажатии методом POST -->
+
   <?php
-    $jopa1 = "Hello";
+  // Подключаем файл с логикой из папки includes
+  include __DIR__ . '/includes/logic.php';
 
-    echo "var: $jopa1"."<br>";
-    echo 'var: $jopa'."<br>";
-    echo "<input type='text'>"."<br>";
+  // Отображаем результат
+  if ($Jopa !== null):
+    echo "<p>Randomated number: $Jopa </p><br>";
+  endif;
 
-    $jopa2 = strlen($jopa1);
-    
-    echo $jopa2."<br>";
+  // Подключаем файл для обработки результата
+  include __DIR__ . '/includes/messages.php';
   ?>
+
 </body>
+
 </html>
